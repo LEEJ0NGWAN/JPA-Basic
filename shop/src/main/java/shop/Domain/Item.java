915b0ebafe0f19619a8b3d4b9b88ba93dd055ca1
LEEJ0NGWAN <lastffx@gmail.com>
2022-03-17 14:54:58 +0900

    refactor: column names

--- shop/src/main/java/shop/Domain/Item.java
+++ shop/src/main/java/shop/Domain/Item.java
@@ -28,6 +28,9 @@ public class Item {
     private List<OrderItem> orderItems = new ArrayList<>();
 
     @ManyToMany
-    @JoinTable(name = "ITEM_CATEGORY")
+    @JoinTable(
+            name = "ITEM_CATEGORY",
+            joinColumns = @JoinColumn(name = "ITEM_ID"),
+            inverseJoinColumns = @JoinColumn(name = "CATEGORY_ID"))
     private List<Category> categories = new ArrayList<>();
 }
